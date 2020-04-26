e31b97f03bb84cba0d83068c1850ea893643b000
Eder Christian <ederchristian92@gmail.com>
2020-04-26 18:03:47 -0300

    style: add the initial layout for the entire project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import React from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import styled, { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #000;
+    color: #FFF;
+    max-width: 90%;
+    margin: auto;
+    font-weight: 300;
+  }
+
+  @media screen and (min-width: 1100px) {
+    body {
+      max-width: 1200px;
+    }
+  }
+`
+
+const Header = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: 16px 0;
+
+  @media screen and (min-width: 1100px) {
+    padding: 32px 0;
+  }
+`
+
+const Title = styled.h1`
+  font-family: "Montserrat", sans-serif;
+  font-size: 28px;
+  font-weight: 700;
+  padding-bottom: 16px;
+
+  @media screen and (min-width: 1100px) {
+    font-size: 34px;
+  }
+`
+
+const Subtitle = styled.h2`
+  font-family: "Roboto", sans-serif;
+  font-size: 16px;
+  line-height: 1.6;
+  padding-bottom: 32px;
+
+  @media screen and (min-width: 1100px) {
+    font-size: 20px;
+    max-width: 600px;
+    margin: auto;
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <GlobalStyle />
+      <Header>
+        <Title>Stories do Pai</Title>
+        <Subtitle>
+          Gere alguns dos maiores ensinamentos de 12 anos de experiência de
+          Ícaro de Carvalho
+        </Subtitle>
+      </Header>
+    </React.Fragment>
   )
 }
 
