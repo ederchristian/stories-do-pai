fdf79e751358c3ad1d4ce3f2dac4477594f4f652
Eder Christian <ederchristian92@gmail.com>
2020-05-03 13:02:30 -0300

    Updates

---
+++ precache-manifest.086843dabc7b9fb31c97d8389066c1b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296c97bd1b17f835593fb7cb9f59a2b8",
+    "url": "/stories-do-pai/index.html"
+  },
+  {
+    "revision": "a3f961112139794426cf",
+    "url": "/stories-do-pai/static/js/2.a3eb9d2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/stories-do-pai/static/js/2.a3eb9d2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f67559b1d17d514627e9",
+    "url": "/stories-do-pai/static/js/main.d7259a53.chunk.js"
+  },
+  {
+    "revision": "ca31b6beab5558b9e341",
+    "url": "/stories-do-pai/static/js/runtime-main.bdfcbd22.js"
+  },
+  {
+    "revision": "47dc1b9150ba71b96e86af414e972b39",
+    "url": "/stories-do-pai/static/media/icaro-de-carvalho.47dc1b91.jpg"
+  },
+  {
+    "revision": "cd2a8aa74d97d49c7b4a5d21a8abd440",
+    "url": "/stories-do-pai/static/media/iphone-x.cd2a8aa7.svg"
+  }
+]);
