ca77163f617e18f43939f59a059b7b8f2bfa5681
Eder Christian <ederchristian92@gmail.com>
2021-02-16 00:07:09 -0300

    feat: add the Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import * as Styled from "./styled"
+
+export default function Header() {
+  return (
+    <Styled.Header>
+      <Styled.Title>Stories do Pai</Styled.Title>
+      <Styled.Subtitle>
+        Gere stories do Instagram com alguns dos maiores ensinamentos de +12
+        anos de experiência de Ícaro de Carvalho
+      </Styled.Subtitle>
+    </Styled.Header>
+  )
+}
