b5244d58a3d1be0375038f1cc9e2f608cb72455b
Eder Christian <ederchristian92@gmail.com>
2021-03-27 22:06:40 -0300

    refactor: remove unused component

--- src/App.js
+++ src/App.js
@@ -3,23 +3,13 @@ import React from "react"
 import Main from "./pages/Main"
 
 import { GlobalStyle } from "./components/Styled/global"
-import { LinkGitHub } from "./style"
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <GlobalStyle />
-
-      <LinkGitHub
-        href="https://github.com/ederchristian/stories-do-pai"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        Ver no GitHub
-      </LinkGitHub>
-
       <Main />
-    </React.Fragment>
+    </>
   )
 }
 
