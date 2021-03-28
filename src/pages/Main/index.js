6282cd91531cf9ce19086de66ebcf421102f9740
Eder Christian <ederchristian92@gmail.com>
2021-03-27 22:07:38 -0300

    feat: add a new logo

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 
+import LogoEder from "../../static/assets/svg/logo-eder-christian.svg"
+
 import Header from "../../components/Header"
 import RandomStory from "../../components/RandomStory"
 
@@ -8,6 +10,7 @@ import * as Styled from "./styled"
 function App() {
   return (
     <Styled.Main>
+      <Styled.Logo src={LogoEder} alt="Logo Eder Christian" />
       <Header />
       <RandomStory />
     </Styled.Main>
--- src/pages/Main/styled.js
+++ src/pages/Main/styled.js
@@ -1,10 +1,25 @@
 import styled from "styled-components"
 
 export const Main = styled.div`
+  position: relative;
+  text-align: center;
+
   @media screen and (min-width: 1100px) {
     display: flex;
     align-items: center;
-    justify-content: space-around;
+    justify-content: space-between;
     width: 100%;
+  }
+`
+
+export const Logo = styled.img`
+  width: 150px;
+  margin-bottom: 32px;
+
+  @media screen and (min-width: 1100px) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    margin-bottom: 0;
   }
 `
